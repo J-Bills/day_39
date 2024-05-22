226f029286286f9f456544610194115b111a92d0
J-Bills <billsjamal@gmail.com>
2024-05-22 12:06:17 -0700

    initial commit

--- flight-deals-start/notification_manager.py
+++ flight-deals-start/notification_manager.py
@@ -1,3 +1,19 @@
+from twilio.rest import Client
+import os
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
+    def __init__(self) -> None:
+        
+        self.account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        self.auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        self.twilio_num = os.environ['TWILIO_NUM']
+        self.client = Client(self.account_sid, self.auth_token)
+    
+    def generating_message(self):
+        message = self.client.messages \
+            .create(
+                body=full_text,
+                from_=twilio_num,
+                to='+11111111'
+            )
+        #This class is responsible for sending notifications with the deal flight details.
     pass
